ca5780a84701d7c61751f7a796f07d0d44e690a7
SamGiles <aphro.cag@googlemail.com>
2013-05-20 20:31:53 +0100

    If user has no searches saved send 404

--- routes/v1/user/searches.js
+++ routes/v1/user/searches.js
@@ -8,7 +8,13 @@ module.exports = function(req, res){
         var erightsid = req.params.id;
 
         search.get(erightsid, function(searches) {
-                res.send(JSON.stringify(searches.getTerms()));
+                var terms = searches.getTerms();
+
+                if (terms.length === 0) {
+                        res.status(404).send("(404) Not Found");
+                } else {
+                        res.send(JSON.stringify(searches.getTerms()));
+                }
         });
 };
 
