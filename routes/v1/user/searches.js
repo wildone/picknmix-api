2046f847dfe6808ac7a4dc8e5fe2a751daafb9dc
Luke Blaney <git@lukeblaney.co.uk>
2013-05-20 19:20:25 +0100

    add more sample data

--- routes/v1/user/searches.js
+++ routes/v1/user/searches.js
@@ -6,9 +6,19 @@ module.exports = function(req, res){
 	var sampledata = [
 		{
 			id: 1,
-			term: "city:london",
+			term: "London",
 			label: "London"
-		}
+		},
+		{
+			id: 2,
+			term: "Syrian Electronic Army",
+			label: "Syrian Electronic Army"
+		},
+		{
+			id: 3,
+			term: "Page:fcdae4e8-cd25-11de-a748-00144feabdc0",
+			label: "Management"
+		},
 	];
   res.send(JSON.stringify(sampledata));
 };
