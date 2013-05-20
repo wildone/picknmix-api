10257fec4c0e23c3e1318f572d702602a56a78e2
SamGiles <aphro.cag@googlemail.com>
2013-05-20 20:22:20 +0100

    Use the component instead of sample data

--- routes/v1/user/searches.js
+++ routes/v1/user/searches.js
@@ -1,25 +1,14 @@
 /*
  * GET searches for a given user
  */
+var search = require("picknmix-savedsearches");
 
 module.exports = function(req, res){
-	var sampledata = [
-		{
-			id: 1,
-			term: "London",
-			label: "London"
-		},
-		{
-			id: 2,
-			term: "Syrian Electronic Army",
-			label: "Syrian Electronic Army"
-		},
-		{
-			id: 3,
-			term: "Page:fcdae4e8-cd25-11de-a748-00144feabdc0",
-			label: "Management"
-		},
-	];
-  res.send(JSON.stringify(sampledata));
+
+        var erightsid = req.params.id;
+
+        search.get(erightsid, function(searches) {
+                res.send(JSON.stringify(searches.getTerms()));
+        });
 };
 
