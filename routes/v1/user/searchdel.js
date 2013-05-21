abf343eb199f0deac6d1d3ecb067e41d93944b5c
SamGiles <aphro.cag@googlemail.com>
2013-05-21 14:11:26 +0100

    Futurise delete

--- routes/v1/user/searchdel.js
+++ routes/v1/user/searchdel.js
@@ -6,9 +6,13 @@ module.exports = function(req, res) {
             term = req.params.term;
 
         search.get(erightsid, function(searches) {
-                searches.removeSearch({ "term": term });
-                res.status(204).send();
-                searches.destroy();
-                varnish.purge(req.host, "/v1/user/"+erightsid+".*");
+                searches.removeSearch({ "term": term }).onSuccess(function(value) {
+                        res.status(204).send();
+                        searches.destroy();
+                        varnish.purge(req.host, "/v1/user/"+erightsid+".*");
+                }).onError(function(value) {
+                        res.status(500).send();
+                        searches.destroy();
+                });
         });
 }
